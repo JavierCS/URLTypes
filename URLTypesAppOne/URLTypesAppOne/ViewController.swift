b78b80857258d3766dd070afad56f7fb3d45a4b1
Javier Cruz Santiago <javiercruzsantiago@gmail.com>
2021-10-29 17:06:49 -0500

    [INIT] - Initial Commit

---
+++ URLTypesAppOne/URLTypesAppOne/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  URLTypesAppOne
+//
+//  Created by jcruzsa on 23/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTouchOpenAppTwoButton(_ sender: UIButton) {
+        guard let appTwoUrl = URL(string: "appTwo://"),
+              UIApplication.shared.canOpenURL(appTwoUrl) else {
+            let alertController = UIAlertController(title: "Ups", message: "App Two no está instalada en ese dispositivo", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.navigationController?.present(alertController, animated: true)
+            return
+        }
+        UIApplication.shared.open(appTwoUrl)
+    }
+    
+}
+
---
+++ URLTypesAppTwo/URLTypesAppTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLTypesAppTwo
+//
+//  Created by jcruzsa on 23/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
